440259f2014f1b5298a0b18bdad1a9518a3ec14a
lizimo061 <zimol@andrew.cmu.edu>
2018-08-28 16:04:37 -0400

    Modified transformation and no threading

--- src/SurfReg.cpp
+++ src/SurfReg.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/console/parse.h>
@@ -10,9 +11,9 @@
 
 pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr rCloud (new pcl::PointCloud<pcl::PointXYZRGBNormal>);
 pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr mCloud (new pcl::PointCloud<pcl::PointXYZRGBNormal>);
-ThreadMutexObject<bool> done(false);
-ThreadMutexObject<int> count(0);
-ThreadMutexObject<int> iteration(0);
+int count = 0;
+
+pcl::PointCloud<pcl::PointXYZI>::Ptr evaCloud (new pcl::PointCloud<pcl::PointXYZI>);
 
 float getScore(pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr input)
 {
@@ -24,12 +25,24 @@ float getScore(pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr input)
 
     double totalSum = 0;
 
-    count.assignValue(0);
+    //count.assignValue(0);
 
     for(size_t i = 0; i < input->size(); i++)
     {
         tree->nearestKSearch(input->at(i), 1, pointIdxNKNSearch, pointNKNSquaredDistance);
+        pcl::PointXYZI tmppt;
+        tmppt.x = input->at(i).x;// TODO
+        tmppt.y = input->at(i).y;
+        tmppt.z = input->at(i).z;
+        if(sqrt(pointNKNSquaredDistance.at(0)) > 0.2){
+            tmppt.intensity = -1;
+            evaCloud->points.push_back(tmppt);
+            continue;
+        }
+
         totalSum += sqrt(pointNKNSquaredDistance.at(0));
+        tmppt.intensity = sqrt(pointNKNSquaredDistance.at(0));
+        evaCloud->points.push_back(tmppt);
         count++;
     }
 
@@ -40,28 +53,28 @@ void computeAlignment()
 {
     float value = getScore(rCloud);
 
-    if(value < 0.05)
-    {
-        pcl::IterativeClosestPoint<pcl::PointXYZRGBNormal, pcl::PointXYZRGBNormal> icp;
+    // if(value < 0.05)
+    // {
+    //     pcl::IterativeClosestPoint<pcl::PointXYZRGBNormal, pcl::PointXYZRGBNormal> icp;
 
-        pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr aligned (new pcl::PointCloud <pcl::PointXYZRGBNormal>);
+    //     pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr aligned (new pcl::PointCloud <pcl::PointXYZRGBNormal>);
 
-        icp.setInputSource(rCloud);
-        icp.setInputTarget(mCloud);
-        icp.setMaximumIterations(1);
+    //     icp.setInputSource(rCloud);
+    //     icp.setInputTarget(mCloud);
+    //     icp.setMaximumIterations(1);
 
-        for(int i = 0; i < 10; i++)
-        {
-            icp.align(*aligned, icp.getFinalTransformation());
-            iteration++;
-        }
+    //     for(int i = 0; i < 10; i++)
+    //     {
+    //         icp.align(*aligned, icp.getFinalTransformation());
+    //         iteration++;
+    //     }
 
-        value = std::min(getScore(aligned), value);
-    }
+    //     value = std::min(getScore(aligned), value);
+    // }
 
-    std::cout << value << std::endl;
+    std::cout << "diff per point: " << value << std::endl;
 
-    done.assignValue(true);
+    //done.assignValue(true);
 }
 
 int main(int argc, char ** argv)
@@ -74,6 +87,7 @@ int main(int argc, char ** argv)
     std::string modelFile;
     pcl::console::parse_argument(argc, argv, "-m", modelFile);
 
+
     if(reconstructionFile.length() == 0 || modelFile.length() == 0)
     {
         std::cout << "Please provide input files with -r and -m" << std::endl;
@@ -83,6 +97,7 @@ int main(int argc, char ** argv)
     pcl::PCLPointCloud2 rPoints;
 
     pcl::toPCLPointCloud2(*rCloud, rPoints);
+    std::cout << rPoints.fields.size() << std::endl;
 
     for(int i = 0; i < rPoints.fields.size(); i++)
     {
@@ -113,42 +128,6 @@ int main(int argc, char ** argv)
 
     pcl::fromPCLPointCloud2(mPoints, *mCloud);
 
-    int trajectory = 0;
-    pcl::console::parse_argument(argc, argv, "-t", trajectory);
-
-    Eigen::Matrix4f trans;
-
-    if(trajectory == 0)
-    {
-        trans << 0.999759, -0.000287637, 0.0219655, -1.36022,
-                 0.000160294, 0.999983, 0.00579897, 1.48382,
-                 0.0219668, 0.00579404, -0.999742, 1.44256,
-                 0, 0, 0, 1;
-    }
-    else if(trajectory == 1)
-    {
-        trans << 0.99975, -0.00789018, 0.0209474, -0.0976324,
-                 0.00789931, 0.999969, -0.000353282, 1.27618,
-                 0.0209439, -0.000518671, -0.999781, 0.0983734,
-                 0, 0, 0, 1;
-    }
-    else if(trajectory == 2)
-    {
-        trans << 0.999822, 0.0034419, 0.0185526, -0.786316,
-                 -0.00350915, 0.999987, 0.00359374, 1.28433,
-                 0.01854, 0.00365819, -0.999821, 1.45583,
-                 0, 0, 0, 1;
-    }
-    else if(trajectory == 3)
-    {
-        trans << 0.999778, -0.000715914, 0.0210893, -1.13311,
-                 0.000583688, 0.99998, 0.0062754, 1.26825,
-                 0.0210934, 0.0062617, -0.999758, 0.901866,
-                 0, 0, 0, 1;
-    }
-
-    pcl::transformPointCloud(*rCloud, *rCloud, trans);
-
     float radius = 10.0f;
     float theta = 0.0f;
     float phi = 0.0f;
@@ -176,44 +155,51 @@ int main(int argc, char ** argv)
     cloudViewer.addPointCloud<pcl::PointXYZRGBNormal>(mCloud, colorM, "MCloud");
     cloudViewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_OPACITY, 0.1, "MCloud");
 
-    boost::thread * computeThread = new boost::thread(computeAlignment);
 
-    while(!done.getValue())
+    cloudViewer.spinOnce(1, true);
+    cloudViewer.removeShape("text");
+
+    int countVal = count;
+    int sizeInput = rCloud->size();
+
+        //std::cout << "Count number: " << countVal << std::endl; 
+        //std::cout << "totla number: " << sizeInput << " " << countVal/sizeInput << std::endl;
+    if(countVal == rCloud->size())
     {
-        cloudViewer.spinOnce(1, true);
-        cloudViewer.removeShape("text");
+        std::stringstream strs;
 
-        int countVal = count.getValue();
+        //strs << "Aligning... " << iteration.getValue() << "/" << 10;
 
-        if(countVal == rCloud->size())
-        {
-            std::stringstream strs;
+        cloudViewer.addText(strs.str(), 20, 20, 50, 1, 0, 0, "text");
+    }
+    else
+    {
+        std::stringstream strs;
 
-            strs << "Aligning... " << iteration.getValue() << "/" << 10;
+        strs << "Scoring... " << countVal << "/" << rCloud->size();
 
-            cloudViewer.addText(strs.str(), 20, 20, 50, 1, 0, 0, "text");
-        }
-        else
-        {
-            std::stringstream strs;
-
-            strs << "Scoring... " << countVal << "/" << rCloud->size();
-
-            cloudViewer.addText(strs.str(), 20, 20, 50, 1, 0, 0, "text");
-        }
-
-        cloudViewer.setCameraPosition(radius * sin(theta) * cos(phi),
-                                      radius * sin(theta) * sin(phi),
-                                      radius * cos(theta),
-                                      0,
-                                      1,
-                                      0);
-
-        theta += 0.005;
-        phi += 0.005;
+        cloudViewer.addText(strs.str(), 20, 20, 50, 1, 0, 0, "text");
     }
 
-    computeThread->join();
+    cloudViewer.setCameraPosition(radius * sin(theta) * cos(phi),
+                                  radius * sin(theta) * sin(phi),
+                                  radius * cos(theta),
+                                  0,
+                                  1,
+                                  0);
+    theta += 0.005;
+    phi += 0.005;
 
-    delete computeThread;
+    computeAlignment();
+    // computeThread->join();
+
+    // delete computeThread;
+    std::ofstream outfile;
+    outfile.open("error.txt");
+    for(int i=0;i < evaCloud->size(); i++){
+        outfile << evaCloud->at(i).x << " " << evaCloud->at(i).y << " "
+        << evaCloud->at(i).z << " " << evaCloud->at(i).intensity << "\n";
+    }
+    outfile.close();
+
 }
